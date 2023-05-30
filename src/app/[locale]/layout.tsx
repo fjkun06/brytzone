e939f68789f19da3237f70645fc91dd7a6cc4575
= <=>
2023-05-30 12:18:25 +0100

    Added framer-motion

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,8 @@
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
+import "../../styles/main.scss";
+import Image from "next/image";
+import logo from "../icon.png";
 type LayoutProps = {
   children: React.ReactNode;
   params: {
@@ -7,8 +10,13 @@ type LayoutProps = {
   };
 };
 export function generateStaticParams() {
-  return [{ locale: "en" }, { locale: "de" },{ locale: "fr" }];
+  return [{ locale: "en" }, { locale: "de" }, { locale: "fr" }];
 }
+
+export const metadata = {
+  title: "Brtyzone",
+  description: "our proposed solutions to common digital problems",
+};
 
 export default async function LocaleLayout({ children, params: { locale } }: LayoutProps) {
   let messages;
@@ -22,9 +30,14 @@ export default async function LocaleLayout({ children, params: { locale } }: Lay
     <html lang={locale}>
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
-          <nav>hello</nav>
-          {children}
-          <footer>hell-o</footer>
+          <main id="layout">
+            <nav>
+              hello
+              <Image src={logo} alt="" />
+            </nav>
+            <section id="layoutwwww">{children}</section>
+            <footer>hell-o</footer>
+          </main>
         </NextIntlClientProvider>
       </body>
     </html>
