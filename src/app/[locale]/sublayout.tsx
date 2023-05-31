121e726de386d595ed5705622cc5af173ef5a91c
= <=>
2023-05-31 06:26:51 +0100

    Completed moble -> tablet navbar

--- src/app/[locale]/sublayout.tsx
+++ src/app/[locale]/sublayout.tsx
@@ -1,21 +1,44 @@
-'use client'
-import Navbar from '@/stories/layout/navbar/Navbar';
-import React from 'react'
-interface SubLayoutProps{
+"use client";
+import Navbar from "@/stories/layout/navbar/Navbar";
+import { motion, AnimatePresence } from "framer-motion";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { nanoid } from "nanoid";
+import React from "react";
+interface SubLayoutProps {
   children?: React.ReactNode;
-  
 }
-const SubLayout:React.FC<SubLayoutProps> = ({children}) => {
-  const [isOpen, setIsOpen] = React.useState(true)
+const SubLayout: React.FC<SubLayoutProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(true);
+  const min980 = useMediaQuery("(width > 960px)");
 
-  const handleIsOpen = () => setIsOpen(!isOpen)
+  const handleIsOpen = () => setIsOpen(!isOpen);
   return (
-    <main id='layout'>
-       <Navbar handleClick={handleIsOpen} isOpen={isOpen} />
-            <section id="layoutwwww">{children}</section>
-            <footer>hell-o</footer>
+    <main id="layout">
+      <Navbar handleClick={handleIsOpen} isOpen={isOpen} desktop={min980} />
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key={nanoid()}
+            onClick={() => setIsOpen(false)}
+            id="modal"
+            layout
+            animate={{ height: min980 ? "0rem" : "100vh" }}
+            transition={{
+              delay: 0.35,
+            }}
+            exit={{
+              height: 0,
+              transition: {
+                delay: 0.35, ease: "easeInOut"
+              },
+            }}
+          />
+        )}
+      </AnimatePresence>
+      <section id="layoutwwww">{children}</section>
+      <footer>hell-o</footer>
     </main>
-  )
-}
+  );
+};
 
-export default SubLayout
+export default SubLayout;
--- src/stories/layout/navbar/Navbar.tsx
+++ src/stories/layout/navbar/Navbar.tsx
@@ -5,31 +5,31 @@ import logo from "../../../../public/assets/images/icon.png";
 import { motion, AnimatePresence } from "framer-motion";
 import Menu from "./Menu";
 import CloseIcon from "./CloseIcon";
-import useMediaQuery from "@/hooks/useMediaQuery";
 import { NavLink } from "@/stories/NavLink";
 import { nanoid } from "nanoid";
 
 interface NavbarProps {
   /**
    * @param {boolean} [isOpen=false] - Used to check if navbar is open or not.
+   * @param {boolean} [desktop=false] - Used to check if the min width is 960pxt.
    * @param {void} handleClick - Toggle menu state.
    * @param {void} set - Toggle menu state.
    *
    * */
   isOpen: boolean;
+  desktop: boolean;
   handleClick: () => void;
   set?: (x: boolean) => void;
 }
-const Navbar: React.FC<NavbarProps> = ({ isOpen, handleClick }) => {
-  const min980 = useMediaQuery("(width > 960px)");
-  const globalTransition = { stiffness: 100, duration: 0.5 };
+const Navbar: React.FC<NavbarProps> = ({ isOpen, handleClick,desktop }) => {
+  const globalTransition = { stiffness: 100, duration: 0.5, ease: "easeInOut" };
   const routes: string[] = ["home", "internships", "projects", "polls", "about", "contact", "blog"];
 
   console.log(isOpen);
 
   //verifying if element is in viewport
   return (
-    <motion.nav layout animate={{ height: isOpen ? (min980 ? "8rem" : "60rem") : "6.6rem" }} transition={globalTransition}>
+    <motion.nav layout animate={{ height: isOpen ? (desktop ? "8rem" : "67rem") : "9.6rem",paddingTop: isOpen? "3rem":"2.25rem" }} transition={globalTransition}>
       <motion.div>
         <span className="logo">
           <Image src={logo} alt="" />
@@ -37,95 +37,69 @@ const Navbar: React.FC<NavbarProps> = ({ isOpen, handleClick }) => {
         {/* <OpenIcon /> */}
         <Menu isOpen={isOpen} toggleNavbarState={handleClick} />
       </motion.div>
-      {isOpen && (
-        <AnimatePresence>
-          <motion.div
-          layout
-            variants={{
-              open: {
-                x: 0,
-                transition: {
-                  ...globalTransition,
-                  // delayChildren: 10.5,
-                  // staggerChildren: 0.5,
-                  delay: 0.25,
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              layout
+              variants={{
+                open: {
+                  x: 0,
+                  transition: {
+                    ...globalTransition,
+                    delay: 0.25,
+                  },
                 },
-              },
-              closed: {
-                x: "-200%",
-                transition: {
-                  ...globalTransition,
+                closed: {
+                  x: "-200%",
+                  transition: {
+                    ...globalTransition,
+                  },
                 },
-              },
-            }}
-            initial="closed"
-            animate="open"
-            // transition={{ ...globalTransition, delay: 0.25 }}
-            exit="closed"
-            // <motion.div
-            //   variants={{
-            //     open: {
-            //       x: 0,
-            //       transition: {
-            //         type: "spring",
-            //         bounce: 0,
-            //         duration: 0.7,
-            //         delayChildren: 0.3,
-            //         staggerChildren: 0.05,
-            //         delay: 0.25,
-            //       },
-            //     },
-            //     closed: {
-            //       x: "-200%",
-            //       transition: {
-            //         type: "spring",
-            //         bounce: 0,
-            //         duration: 0.3,
-            //       },
-            //     },
-            //   }}
-            //   initial={{ x: "-200%" }}
-            //   animate={{ x: 0 }}
-            //   transition={{ ...globalTransition, delay: 0.25 }}
-            //   exit={{ x: "-200%", transition: { duration: 0.5 } }}
-          >
-            {/* <NavLink href="/" text="home" />
-            <NavLink href="/internships" text="internships" />
-            <NavLink href="/projects" text="projects" />
-            <NavLink href="/polls" text="polls" />
-            <NavLink href="/about" text="about" />
-            <NavLink href="/contact" text="contact" />
-            <NavLink href="/blog" text="blog" /> */}
-            {routes.map((route, index) => (
-              <NavLink i={index} href={`/${route === "home" ? "/" : route}`} text={route} type="mobile" key={nanoid()} />
-            ))}
-          </motion.div>
-          <motion.div
-            key={nanoid()}
-            variants={{
-              open: {
-                // x:  "-200%",
-                x: 0,
-                transition: {
-                  ...globalTransition,
-             
-                  delay: 2.25,
+              }}
+              initial="closed"
+              animate="open"
+              exit="closed"
+           
+            >
+    
+              {routes.map((route, index) => (
+                <NavLink i={index} href={`/${route === "home" ? "/" : route}`} text={route} type="mobile" key={nanoid()} />
+              ))}
+            </motion.div>
+            <motion.div
+              layout
+              key={nanoid()}
+              variants={{
+                open: {
+                  // x:  "-200%",
+                  x: 0,
+                  transition: {
+                    ...globalTransition,
+
+                    delay: 2.25,
+                  },
                 },
-              },
-              closed: {
-                x: "-200%",
-                transition: {
-                  ...globalTransition,
+                closed: {
+                  x: "-200%",
+                  transition: {
+                    ...globalTransition,
+                  },
                 },
-              },
-            }}
-            initial="closed"
-            animate="open"
-          >
-            three
-          </motion.div>
-        </AnimatePresence>
-      )}
+              }}
+              initial="closed"
+              animate="open"
+              exit="closed"
+            >
+              <span>hell-o</span>
+              <span>hell-o</span>
+              <span>hell-o</span>
+              <span>hell-o</span>
+            </motion.div>
+         
+          </>
+        )}
+      </AnimatePresence>
     </motion.nav>
   );
 };
