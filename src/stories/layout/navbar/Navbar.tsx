79ecaf2889473318061d91d8970aa0ff3e2494ba
= <=>
2023-05-31 06:57:55 +0100

    adjusted mobile navbar

--- src/stories/layout/navbar/Navbar.tsx
+++ src/stories/layout/navbar/Navbar.tsx
@@ -21,82 +21,66 @@ interface NavbarProps {
   handleClick: () => void;
   set?: (x: boolean) => void;
 }
-const Navbar: React.FC<NavbarProps> = ({ isOpen, handleClick,desktop }) => {
+const Navbar: React.FC<NavbarProps> = ({ isOpen, handleClick, desktop }) => {
   const globalTransition = { stiffness: 100, duration: 0.5, ease: "easeInOut" };
   const routes: string[] = ["home", "internships", "projects", "polls", "about", "contact", "blog"];
-
-  console.log(isOpen);
-
+  const start = {
+    x: 0,
+    transition: {
+      ...globalTransition,
+      delay: 0.25,
+    },
+  };
+  const end = {
+    x: "-200%",
+    transition: {
+      ...globalTransition,
+    },
+  };
+  const slideIn = desktop
+    ? { open: {}, closed: {} }
+    : {
+        open: start,
+        closed: end,
+      };
+  const slideInn = desktop
+    ? { open: {}, closed: {} }
+    : {
+        open: {
+          ...start,
+          transition: {
+            ...globalTransition,
+            delay: 2.25,
+          },
+        },
+        closed: end,
+      };
+  // if (slideInn.open.transition) slideInn.open.transition.delay = 2.25;
+  // slideInn.open &&  slideInn.open.transition.delay = 2.25;
   //verifying if element is in viewport
   return (
-    <motion.nav layout animate={{ height: isOpen ? (desktop ? "8rem" : "67rem") : "9.6rem",paddingTop: isOpen? "3rem":"2.25rem" }} transition={globalTransition}>
+    <motion.nav layout animate={{ height: isOpen ? (desktop ? "9.6rem" : "70rem") : "9.6rem", paddingTop: isOpen ? "3rem" : "2.25rem" }} transition={globalTransition}>
       <motion.div>
         <span className="logo">
           <Image src={logo} alt="" />
         </span>
-        {/* <OpenIcon /> */}
-        <Menu isOpen={isOpen} toggleNavbarState={handleClick} />
+        {!desktop && <Menu isOpen={isOpen} toggleNavbarState={handleClick} />}
       </motion.div>
       <AnimatePresence>
         {isOpen && (
           <>
-            <motion.div
-              layout
-              variants={{
-                open: {
-                  x: 0,
-                  transition: {
-                    ...globalTransition,
-                    delay: 0.25,
-                  },
-                },
-                closed: {
-                  x: "-200%",
-                  transition: {
-                    ...globalTransition,
-                  },
-                },
-              }}
-              initial="closed"
-              animate="open"
-              exit="closed"
-           
-            >
-    
+            <motion.div layout variants={slideIn} initial="closed" animate="open" exit="closed">
               {routes.map((route, index) => (
-                <NavLink i={index} href={`/${route === "home" ? "/" : route}`} text={route} type="mobile" key={nanoid()} />
+                <NavLink i={index} href={`/${route === "home" ? "/" : route}`} text={route} type={desktop ? "desktop" : "mobile"} key={nanoid()} />
               ))}
             </motion.div>
-            <motion.div
-              layout
-              key={nanoid()}
-              variants={{
-                open: {
-                  // x:  "-200%",
-                  x: 0,
-                  transition: {
-                    ...globalTransition,
-
-                    delay: 2.25,
-                  },
-                },
-                closed: {
-                  x: "-200%",
-                  transition: {
-                    ...globalTransition,
-                  },
-                },
-              }}
-              initial="closed"
-              animate="open"
-              exit="closed"
-            >
+            <motion.div transition={{ delay: 2.25 }} layout key={nanoid()} variants={slideInn} initial="closed" animate="open" exit="closed">
               <span>hell-o</span>
               <span>hell-o</span>
               <span>hell-o</span>
               <span>hell-o</span>
+              <span>theme</span>
             </motion.div>
-         
           </>
         )}
       </AnimatePresence>
