f34a7a0be9386d7a7100f9a553b401563f522bc7
= <=>
2023-06-05 10:36:29 +0100

    Styled footer items

--- src/stories/layout/footer/Footer.tsx
+++ src/stories/layout/footer/Footer.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
 import React from "react";
+import logo from "../../../../public/assets/images/icon.png";
+import FacebookIcon from "@/stories/components/FacebookIcon";
+import TwitterIcon from "@/stories/components/TwitterIcon";
+import LinkedInIcon from "@/stories/components/LinkedInIcon";
 
 const Footer = () => {
   return (
@@ -11,6 +16,36 @@ const Footer = () => {
             <span className="">Internships</span>
             <span className="">Chat</span>
             <span className="">About</span>
+          </div>
+        </div>
+        <div className="brtyzone_footer_links-item">
+          <h4>Useful Links</h4>
+          <div>
+            <span className="">cotsite@ubuea.com</span>
+            <span className="">+237 90067890</span>
+            <span className="">Cameroon, Buea Molyko</span>
+          </div>
+        </div>
+        <div className="brtyzone_footer_links-item">
+          <h4>Useful Links</h4>
+          <div>
+            <span className="">Projects</span>
+            <span className="">Internships</span>
+            <span className="">Chat</span>
+            <span className="">About</span>
+          </div>
+        </div>
+        <div className="brtyzone_footer_links-item">
+          <span className="logo">
+            <Image src={logo} alt="" />
+          </span>
+          <div>
+            <span className="">Follow us on all our social media platforms top get more updates</span>
+            <span className="">
+              <FacebookIcon />
+              <TwitterIcon />
+              <LinkedInIcon />
+            </span>
           </div>
         </div>
       </section>
