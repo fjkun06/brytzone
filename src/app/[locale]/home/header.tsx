5f7776a247e05f98e9b869a9e48670db701788dd
= <=>
2023-06-05 22:06:39 +0100

    updated images

--- src/app/[locale]/home/header.tsx
+++ src/app/[locale]/home/header.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/stories/components/Button";
 import IconForward from "@/stories/components/IconForward";
 import SearchIcon from "@/stories/components/SearchIcon";
+import Image from "next/image";
 import React from "react";
 export const brytzone = "brytzone";
 const Header = () => {
@@ -41,7 +42,24 @@ const Header = () => {
         <span></span>
         <span></span>
       </div>
-      <div className="right"> right</div>
+      <div className="right">
+        {...new Array(4).fill(4).map((_, i) => (
+          <span className="" key={++i}>
+            {/* <Image style={{objectFit: "cover"}} src={`/hd/img${++i}.png`} alt='gallery_image' width={98} height={103} quality={100}/> */}
+            <Image style={{ objectFit: "cover" }} src={`/sharp/${++i}.png`} alt="gallery_image" width={400} height={400} quality={100} />
+          </span>
+        ))}
+        {...new Array(8).fill(8).map((_, i) => <span className="" key={++i}/>)}
+        <span className="play">
+          <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="28.5803" cy="28.0175" r="28.0175" fill="#0A518B" />
+            <path
+              d="M39.0676 27.5116L24.0383 18.6264C22.8171 17.9048 20.947 18.605 20.947 20.3898V38.1559C20.947 39.757 22.6848 40.722 24.0383 39.9193L39.0676 31.0384C40.4082 30.2485 40.4125 28.3015 39.0676 27.5116Z"
+              fill="white"
+            />
+          </svg>
+        </span>
+      </div>
     </header>
   );
 };
