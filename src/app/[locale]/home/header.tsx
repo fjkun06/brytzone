7f849f57544608fc35c52cfdea4a912dd6909371
= <=>
2023-06-06 05:38:01 +0100

    Completed mobile header

--- src/app/[locale]/home/header.tsx
+++ src/app/[locale]/home/header.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/stories/components/Button";
 import IconForward from "@/stories/components/IconForward";
 import SearchIcon from "@/stories/components/SearchIcon";
+import { nanoid } from "nanoid";
 import Image from "next/image";
 import React from "react";
 export const brytzone = "brytzone";
 const Header = () => {
   const [value, setValue] = React.useState(undefined);
+
   return (
     <header className={`${brytzone}_home-header`}>
       <div className="left">
@@ -32,24 +34,39 @@ const Header = () => {
         </div>
       </div>
       <div className="center">
-        {/* <svg id="b5647e2d-1b7d-4220-adbb-0abefa477ceb" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.38 30.5">
-          <path d="M3,5l17,7L37,5" transform="translate(-2.81 -4.54)" fill="none" stroke="#000" stroke-miterlimit="10" />
-          <path d="M3,12.5l17,7,17-7" transform="translate(-2.81 -4.54)" fill="none" stroke="#000" stroke-miterlimit="10" />
-          <path d="M3,20l17,7,17-7" transform="translate(-2.81 -4.54)" fill="none" stroke="#000" stroke-miterlimit="10" />
-          <path d="M3,27.5l17,7,17-7" transform="translate(-2.81 -4.54)" fill="none" stroke="#000" stroke-miterlimit="10" />
-        </svg> */}
         <span></span>
         <span></span>
         <span></span>
       </div>
       <div className="right">
         {...new Array(4).fill(4).map((_, i) => (
-          <span className="" key={++i}>
+          <span className="" key={nanoid()}>
             {/* <Image style={{objectFit: "cover"}} src={`/hd/img${++i}.png`} alt='gallery_image' width={98} height={103} quality={100}/> */}
             <Image style={{ objectFit: "cover" }} src={`/sharp/${++i}.png`} alt="gallery_image" width={400} height={400} quality={100} />
           </span>
         ))}
-        {...new Array(8).fill(8).map((_, i) => <span className="" key={++i}/>)}
+        {...new Array(8).fill(8).map((_, i) => (
+          <span className="" key={nanoid()}>
+            {i === 1 && (
+              <>
+                <span>60+</span>
+                <span>Projects</span>
+              </>
+            )}
+            {i === 2 && (
+              <>
+                <span>40+</span>
+                <span>Tutors</span>
+              </>
+            )}
+            {i === 0 && (
+              <>
+                <span>200+</span>
+                <span>Users</span>
+              </>
+            )}
+          </span>
+        ))}
         <span className="play">
           <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="28.5803" cy="28.0175" r="28.0175" fill="#0A518B" />
