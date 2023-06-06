1581eae05f4382610bfa887e88cc3f2a647f0221
= <=>
2023-06-06 16:10:59 +0100

    completed explore

--- src/utils/sharp.js
+++ src/utils/sharp.js
@@ -2,14 +2,14 @@ const sharp = require('sharp');
 const fs = require('fs');
 const directory = 'public/home/community';
 
-// fs.readdirSync(directory).forEach(file => {
-//   console.log(file);
-//   file === "conv.svg" && sharp(`${directory}/${file}`)
-//     // .resize(1920, 1080) // width, height
-//     .toFile(`${directory}/img11.png`);
-//   });
 fs.readdirSync(directory).forEach(file => {
- sharp(`${directory}/${file}`)
+  console.log(file);
+  file === "phone.png" && sharp(`${directory}/${file}`)
     // .resize(1920, 1080) // width, height
-    .toFile(`${directory}/${file.slice(0,6)}.webp`);
+    .toFile(`${directory}/phone.webp`);
   });
+// fs.readdirSync(directory).forEach(file => {
+//  sharp(`${directory}/${file}`)
+//     // .resize(1920, 1080) // width, height
+//     .toFile(`${directory}/${file.slice(0,6)}.webp`);
+//   });
