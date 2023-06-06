332a39e9c53c32424ee89030180b19aaa45e319b
= <=>
2023-06-06 08:23:36 +0100

    Complteted right

--- src/utils/sharp.js
+++ src/utils/sharp.js
@@ -11,5 +11,5 @@ const directory = 'public/sharp';
 fs.readdirSync(directory).forEach(file => {
  sharp(`${directory}/${file}`)
     // .resize(1920, 1080) // width, height
-    .toFile(`${directory}/${file.slice(0,1)}.png`);
+    .toFile(`${directory}/${file.slice(0,1)}.webp`);
   });
