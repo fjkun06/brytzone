7056dbf5f7941f789896514b3a851c177b471a1c
= <=>
2023-06-06 09:45:51 +0100

    100%

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,10 +3,14 @@ import { Button } from "@/stories/components/Button";
 import IconForward from "@/stories/components/IconForward";
 import UserAddIcon from "@/stories/layout/navbar/UserAddIcon";
 import { useTranslations } from "next-intl";
-import Header, { brytzone } from "./home/header";
+import { brytzone } from "./home/header";
+import dynamic from "next/dynamic";
 
 export default function Index() {
   const t = useTranslations("Index");
+  const Header = dynamic(() => import("./home/header"), {
+    loading: () => <p>Loading Header...</p>,
+  });
   return (
     <section className={`${brytzone}_home`}>
       <Header />
