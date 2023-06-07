fd97066968697fe08efa3bd295916ba06b00f8a0
= <=>
2023-06-07 13:48:00 +0100

    Added How component

--- src/app/[locale]/home/how.tsx
+++ src/app/[locale]/home/how.tsx
@@ -1,7 +1,20 @@
+import Heading from "@/stories/components/heading";
 import React from "react";
+import { brytzone } from "./header";
+import Image from "next/image";
 
 const How = () => {
-  return <div></div>;
+  return (
+    <section className={`${brytzone}_home-how`}>
+      <Heading>Explore your learning area and be like a PRO</Heading>
+      <div className="hleft">
+        svg
+      </div>
+      <div className="hright">
+        <Heading>Explore your learning area and be like a PRO</Heading>
+      </div>
+    </section>
+  );
 };
 
 export default How;
