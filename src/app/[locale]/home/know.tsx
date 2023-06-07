e1814e037a082ddfcc3db697cead6a5933832f3e
= <=>
2023-06-07 10:01:28 +0100

    Loaded all 5 cards

--- src/app/[locale]/home/know.tsx
+++ src/app/[locale]/home/know.tsx
@@ -1,16 +1,51 @@
 import React from "react";
 import { brytzone } from "./header";
 import Image from "next/image";
+import Heading from "@/stories/components/heading";
 
 const Know = () => {
   return (
     <section className={`${brytzone}_home-know`}>
-      <div className="know_card">
-        <div className="know_card-image">
-          <Image src="/home/know/img1.png" width={400} height={400} alt="card_img" />
+      <Heading bordered>Get to know us by chosing a starting point</Heading>
+      <section>
+        <div className="know_card">
+          <div className="know_card-image">
+            <Image src="/home/know/img1.png" width={400} height={400} alt="card_img" />
+          </div>
+          <div className="know_card-text">
+            <h3>Our Approach</h3>
+            <p>We keep you updated with the most reliable information on Campus.</p>
+          </div>
         </div>
-        <div className="know_card-text">text</div>
-      </div>
+        <div className="know_card">
+          <div className="know_card-image"></div>
+          <div className="know_card-text">
+            <h3>Our Approach</h3>
+            <p>We keep you updated with the most reliable information on Campus.</p>
+          </div>
+        </div>
+        <div className="know_card">
+          <div className="know_card-image">
+            <Image src="/home/know/img1.png" width={400} height={400} alt="card_img" />
+          </div>
+          <div className="know_card-text">
+            <h3>Our Approach</h3>
+            <p>We keep you updated with the most reliable information on Campus.</p>
+          </div>
+        </div>
+        <div className="know_card">
+          <div className="know_card-image"></div>
+          <div className="know_card-text">
+            <h3>Our Approach</h3>
+            <p>We keep you updated with the most reliable information on Campus.</p>
+          </div>
+        </div>
+        <div className="know_card">
+          <div className="know_card-image">
+            <Image src="/home/know/img1.png" width={400} height={400} alt="card_img" />
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
