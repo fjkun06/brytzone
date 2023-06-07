52536790f41402f3d794a018cf89916437506092
= <=>
2023-06-07 23:19:29 +0100

    cards left

--- src/app/[locale]/home/meet.tsx
+++ src/app/[locale]/home/meet.tsx
@@ -1,7 +1,106 @@
+import Heading from "@/stories/components/heading";
 import React from "react";
+import { brytzone } from "./header";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import SW, { Autoplay, Pagination, Navigation } from "swiper";
 
 const Meet = () => {
-  return <div></div>;
+  const progressCircle = React.useRef(null);
+  const progressContent = React.useRef(null);
+  const onAutoplayTimeLeft = (s: SW, time: number, progress: number) => {
+    if (progressCircle.current) {
+      const prog: any = progressCircle.current;
+      prog.style.setProperty("--progress", 1 - progress);
+    }
+    if (progressContent.current) {
+      const prog: any = progressContent.current;
+      prog.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+  return (
+    <section className={`${brytzone}_home-meet`}>
+      <Heading bordered>Meet people who can help you</Heading>
+      <p>Meet professionals who can be of great help to you through their works. Just part of our collections who are able to make you grow your skills</p>
+      <div className="meet_cards">
+        <article>
+          <span>pic</span>
+          <span>name</span>
+          <span>title</span>
+          <span>links</span>
+        </article>
+      </div>
+      {/* <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper> */}
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[ Pagination, Navigation]}
+        // modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <article>
+            <span>pic</span>
+            <span>name</span>
+            <span>title</span>
+            <span>links</span>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </section>
+  );
 };
 
 export default Meet;
