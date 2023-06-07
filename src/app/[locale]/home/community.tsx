a0964f48179daee7bb17e005b76372b9f7aeb82c
pjtcoco <zonecoco0@gmail.com>
2023-06-07 21:50:17 +0100

    Added Community Translation

--- src/app/[locale]/home/community.tsx
+++ src/app/[locale]/home/community.tsx
@@ -3,16 +3,25 @@ import { brytzone } from "./header";
 import { Button } from "@/stories/components/Button";
 import Heading from "@/stories/components/heading";
 import Image from "next/image";
-
+import { useTranslations } from "next-intl";
+const communityT = useTranslations('community')
 const Community = () => {
   return (
     <section className={`${brytzone}_home-community`}>
       <div className="">
-        <Image src='/home/community/mockup.webp' width={800} height={800} alt="mockup"/>
+        <Image
+          src="/home/community/mockup.webp"
+          width={800}
+          height={800}
+          alt="mockup"
+        />
       </div>
       <div className="">
-        <Heading>Be a part  of  our community</Heading>
-        <p>Sign up today and get the most out of our site ranging from Internships, Polls, getting access to projects and so much more.....</p>
+        <Heading>{communityT("heading")}</Heading>
+        <p>
+          Sign up today and get the most out of our site ranging from
+          Internships, Polls, getting access to projects and so much more.....
+        </p>
         <Button category="content">Join Us</Button>
       </div>
     </section>
