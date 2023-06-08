9bb6e7b05c689621d1e52232f57abafe396b3c68
= <=>
2023-06-08 08:27:29 +0100

    Completed saying

--- src/app/[locale]/home/saying.tsx
+++ src/app/[locale]/home/saying.tsx
@@ -35,20 +35,22 @@ const Saying = () => {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        // autoplay={{
-        //   delay: 2500,
-        //   disableOnInteraction: false,
-        // }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
+        loop={true}
         navigation={true}
         // modules={[Pagination, Navigation]}
         modules={[Autoplay, Pagination, Navigation]}
         onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
+        speed={1200}
       >
-        {...new Array(10).fill(10).map((el) => (
+        {...new Array(4).fill(4).map((el) => (
           <SwiperSlide key={nanoid()}>
             <article>
               <div>
@@ -61,9 +63,10 @@ const Saying = () => {
                   </svg>
                 </span>
                 <p>
-                  Indeed I’ve been blessed by COT’SITE, considering the fact that I’m being able to do all the things I haven’t been able to do before. indeed I’m really grateful for this opportunity.
+                  Indeed I’ve been blessed by <b>BRYTZONE</b>, considering the fact that I’m being able to do all the things I haven’t been able to do before. indeed I’m really grateful for this
+                  opportunity.
                 </p>
-                <span className='last'>
+                <span className="last">
                   <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                       d="M13.6567 0.719798C14.4301 0.719798 15.1367 0.919797 15.7767 1.3198C16.4167 1.74646 16.9234 2.3198 17.2967 3.0398C17.6701 3.78646 17.8567 4.65313 17.8567 5.6398C17.8567 6.41313 17.6967 7.17313 17.3767 7.9198C17.0567 8.69313 16.6167 9.41313 16.0567 10.0798C15.4701 10.7731 14.7901 11.3731 14.0167 11.8798C13.2434 12.3865 12.4034 12.7731 11.4967 13.0398L9.77675 10.8798C10.8701 10.3998 11.7634 9.82646 12.4567 9.1598C13.1234 8.49313 13.4567 7.9198 13.4567 7.4398C13.4567 7.1198 13.3367 6.82646 13.0967 6.5598C12.8567 6.29313 12.5901 6.02646 12.2967 5.7598C11.6567 5.30646 11.2301 4.8798 11.0167 4.4798C10.7767 4.10646 10.6567 3.69313 10.6567 3.2398C10.6567 2.46646 10.9367 1.85313 11.4967 1.3998C12.0567 0.946464 12.7767 0.719798 13.6567 0.719798ZM4.69675 0.719798C5.47008 0.719798 6.17675 0.919797 6.81675 1.3198C7.45675 1.74646 7.96342 2.3198 8.33675 3.0398C8.71008 3.78646 8.89675 4.65313 8.89675 5.6398C8.89675 6.41313 8.73675 7.17313 8.41675 7.9198C8.09675 8.69313 7.65675 9.41313 7.09675 10.0798C6.51008 10.7731 5.83008 11.3731 5.05675 11.8798C4.28341 12.3865 3.44342 12.7731 2.53675 13.0398L0.816748 10.8798C1.91008 10.3998 2.80341 9.82646 3.49675 9.1598C4.16341 8.49313 4.49675 7.9198 4.49675 7.4398C4.49675 7.1198 4.37675 6.82646 4.13675 6.5598C3.89675 6.29313 3.63008 6.02646 3.33675 5.7598C2.69675 5.30646 2.27008 4.8798 2.05675 4.4798C1.81675 4.10646 1.69675 3.69313 1.69675 3.2398C1.69675 2.46646 1.97675 1.85313 2.53675 1.3998C3.09675 0.946464 3.81675 0.719798 4.69675 0.719798Z"
@@ -71,7 +74,7 @@ const Saying = () => {
                     />
                   </svg>
                 </span>
-                <Image src='/home/saying/girl.png' width={390} height={480} alt="girl_image" />
+                <Image src="/home/saying/girlmini.png" width={390} height={480} alt="girl_image" />
               </div>
 
               <div>
@@ -79,6 +82,7 @@ const Saying = () => {
                 <span>Computer Engineering Student</span>
               </div>
             </article>
+            <Image src="/home/saying/girl.png" width={390} height={480} alt="girl_image" />
           </SwiperSlide>
         ))}
 
