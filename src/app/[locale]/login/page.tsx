44dddbd08ee89604f7f849d3621194a5bb126e41
= <=>
2023-06-26 20:21:12 +0100

    initialised login

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -8,6 +8,7 @@ export const metadata = {
   description: "section displaying all the services we offer",
 };
 import Link from "next-intl/link";
+import { brytzone } from "../home/header";
 
 const Polls = () => {
   // const t = useTranslations("dash");
@@ -15,18 +16,17 @@ const Polls = () => {
   console.log("path: ", path1);
   console.log(path1.slice(3 - path1.length));
   // console.log(path.slice(3 - path.length) + "dashboard");
-const p2 = path1.slice(3 - path1.length)
+  const p2 = path1.slice(3 - path1.length);
   return (
-    <div>
-      {/* <h1>{t("title")}</h1> */}
-      <h1>Login</h1>
-      {/* // When the user is on `/en`, the link will point to `/en/about` */}
-      <Link href="/about">About</Link>
-      {/* // You can override the `locale` to switch to another languag */}
-      <Link href={'login'} locale="de">
-        Switch to German
-      </Link>
-    </div>
+    <section className={`${brytzone}_login`}>
+      <div>
+        <h1>Login</h1>
+        <Link href="/about">About</Link>
+        <Link href={"login"} locale="de">
+          Switch to German
+        </Link>
+      </div>
+    </section>
   );
 };
 
