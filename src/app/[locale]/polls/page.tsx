982cb544c1e142bcf718c2f068af6ecf8ca03db3
= <=>
2023-07-03 15:52:16 +0100

    Adding search loader

--- src/app/[locale]/polls/page.tsx
+++ src/app/[locale]/polls/page.tsx
@@ -7,6 +7,9 @@ import { brytzone } from "../home/header";
 import GridImage from "./grid";
 import { Button } from "@/stories/components/Button";
 import React from "react";
+import Heading from "@/stories/components/heading";
+import SearchIcon from "@/stories/components/SearchIcon";
+import { CircleLoader, PulseLoader } from "react-spinners";
 export const metadata = {
   title: "Ensome | Services",
   description: "section displaying all the services we offer",
@@ -19,6 +22,18 @@ const Polls = () => {
   console.log(path1.slice(3 - path1.length));
   // console.log(path.slice(3 - path.length) + "dashboard");
   const [visible, setVisible] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [query, setQuery] = React.useState("");
+
+  const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+    setQuery(e.target.value);
+    if (e.target.value.length > 0) {
+      setIsLoading(true);
+    } else {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className={`${brytzone}_polls`}>
@@ -31,8 +46,7 @@ const Polls = () => {
               Cure Your <span>Curiosity</span>
             </h2>
             <p>
-              Have most of your <span>worries answered</span> through our online <span>Polls.</span> all you need do is to select your Level, semester,Course and then answer the questions that
-              follows.
+              Have most of your <span>worries answered</span> through our online <span>Polls.</span> all you need do is to select your Level, semester, course and answer the questions that follow.
             </p>
             <Button>Be Part Now</Button>
           </article>
@@ -41,6 +55,15 @@ const Polls = () => {
           </motion.div>
         </section>
       </header>
+      <section className="search">
+        <Heading bordered>Check Out Available Polls</Heading>
+
+        <p>Hurray!!!..........Polls on various courses are now available based on your current department, semester and level</p>
+        <div className="search-bar">
+          {isLoading ? <PulseLoader loading={isLoading} color="grey" aria-label="Loading Spinner" data-testid="loader" className="wave" /> : <SearchIcon />}
+          <input onChange={handleSearch} value={query} type="search" placeholder="Search a poll" />
+        </div>
+      </section>
     </section>
   );
 };
