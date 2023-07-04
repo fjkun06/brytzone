3d532e9feedde5cefdcbb4851f73ee6b46dbb3c8
= <=>
2023-07-04 01:22:20 +0100

    styling cards

--- src/app/[locale]/polls/[id]/page.tsx
+++ src/app/[locale]/polls/[id]/page.tsx
@@ -6,6 +6,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import { brytzone } from "../../home/header";
+import Heading from "@/stories/components/heading";
 export default function Page({ params }: { params: { username: string } }) {
   const router = useRouter();
   useEffect(() => {
@@ -15,10 +16,18 @@ export default function Page({ params }: { params: { username: string } }) {
   return (
     <section className={`${brytzone}_poll`}>
       <header>
-        <h2></h2>
+        <h2>COURSE: CEC 320,EMBEDDED SYSTEMS</h2>
       </header>
       <section className="content">
-        <div className="introduction"></div>
+        <div className="introduction">
+          <Heading bordered>Answer The questions</Heading>
+          <p>
+            <span>This section contains all the modules of this course and requires you to answer a</span> <span className="tagged">Yes</span> or
+            <span>
+              <span className="tagged"> No</span> to every question that requires such answer
+            </span>
+          </p>
+        </div>
         <div className="cards">
           <div className="card">
             <span className="index">1</span>
@@ -33,7 +42,7 @@ export default function Page({ params }: { params: { username: string } }) {
                     <input type="checkbox" name="" placeholder="" />
                   </label>
                   <label>
-                    No
+                     No
                     <input type="checkbox" name="" placeholder="" />
                   </label>
                 </div>
